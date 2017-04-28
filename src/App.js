d18765e3b089616aef96cd270e71c7948935c9dc
elzup <guild0105@gmail.com>
2017-04-29 01:30:48 +0900

    Based

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import moment from 'moment';
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      now: moment(),
+    };
+  }
+
+  tick() {
+    this.setState({ now: this.state.now.add({ s: 1 }) });
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(this.tick.bind(this), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h1>Dendai Time's Gate</h1>
+          <h2>時間を統べる</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>{ this.state.now.format('MM/DD HH:mm:ss') }</div>
       </div>
     );
   }
