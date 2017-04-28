783f131b600ff1aa5b599030611d792af3173064
elzup <guild0105@gmail.com>
2017-04-29 01:50:19 +0900

    v1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './App.css';
 import moment from 'moment';
+import _ from 'lodash';
+import { Nav, NavItem } from 'react-bootstrap'
+
+import timeData from './data/time.json';
 
 class App extends React.Component {
   constructor(props) {
@@ -23,13 +27,19 @@ class App extends React.Component {
   }
 
   render() {
+    const basicList = _.map(timeData, (e, k) => (
+      <li><span>{k}限</span>{e.start} - {e.end}</li>
+    ));
     return (
       <div className="App">
         <div className="App-header">
           <h1>Dendai Time's Gate</h1>
           <h2>時間を統べる</h2>
         </div>
-        <div>{ this.state.now.format('MM/DD HH:mm:ss') }</div>
+        <h2 className="head-time">{ this.state.now.format('MM/DD HH:mm:ss') }</h2>
+        <ul className="basic-list">
+          {basicList}
+        </ul>
       </div>
     );
   }
