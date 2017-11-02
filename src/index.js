4dbdd172fac0750acbc8db2be623781d6029ee64
elzup <guild0105@gmail.com>
2017-11-02 17:49:35 +0900

    Prettier

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+// @flow
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './index.css'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'))
