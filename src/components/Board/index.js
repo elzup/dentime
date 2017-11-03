1383b03c5e225af08c375e3edc3be7bd03f5e365
elzup <guild0105@gmail.com>
2017-11-03 12:03:41 +0900

    Style TimeRows

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -4,15 +4,21 @@ import React from 'react'
 import TimeRow from '../TimeRow'
 import type { Period } from '../../types'
 
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+	text-align: center;
+`
+
 const Board = ({ periods }: { periods: Period[] }) => {
 	return (
-		<div className="basic-list">
+		<Wrapper>
 			<p>開館 07:30</p>
 			{periods.map(period => (
 				<TimeRow key={period.info.period} period={period} />
 			))}
 			<p>閉館 22:30</p>
-		</div>
+		</Wrapper>
 	)
 }
 
--- src/components/TimeRow/index.js
+++ src/components/TimeRow/index.js
@@ -1,14 +1,82 @@
 // @flow
 
 import React from 'react'
-import type { Period } from '../../types'
+import type {
+	Period,
+	PeriodStatus,
+	PeriodStatusBefore,
+	PeriodStatusFinish,
+	PeriodStatusProgress,
+} from '../../types'
+
+import styled from 'styled-components'
+
+const Row = styled.div`
+	display: flex;
+	text-align: center;
+	font-size: 1.5em;
+	justify-content: center;
+	padding: 2.5px 0;
+`
+
+const StRow = styled.div`
+	display: flex;
+`
+
+const StatusLabel = styled.div`
+	width: 100px;
+	width: 3em;
+	color: ${p => p.color};
+	text-align: left;
+`
+
+const Opt = styled.span`
+	font-size: 0.5em;
+	width: 4em;
+`
+
+const TimeStatusBefore = ({ st }: { st: PeriodStatusBefore }) => (
+	<StRow>
+		<StatusLabel />
+		<Opt>{st.fromNowStr}</Opt>
+	</StRow>
+)
+const TimeStatusProgress = ({ st }: { st: PeriodStatusProgress }) => (
+	<StRow>
+		<StatusLabel color={'red'}>Now</StatusLabel>
+		<Opt />
+	</StRow>
+)
+const TimeStatusFinish = ({ st }: { st: PeriodStatusFinish }) => (
+	<StRow>
+		<StatusLabel color={'gray'}>Fin</StatusLabel>
+		<Opt />
+	</StRow>
+)
+
+function getStatus(st: PeriodStatus) {
+	if (st === null) {
+		return null
+	}
+	switch (st.type) {
+		case 'before':
+			return <TimeStatusBefore st={st} />
+		case 'progress':
+			return <TimeStatusProgress st={st} />
+		case 'finish':
+			return <TimeStatusFinish st={st} />
+		default:
+			return null
+	}
+}
 
 const TimeRow = ({ period }: { period: Period }) => (
-	<div className="time-row">
+	<Row>
 		<div>{period.info.period}限</div>
 		<div>
 			{period.start.format('HH:mm')} - {period.end.format('HH:mm')}
 		</div>
-	</div>
+		<div>{getStatus(period.status)}</div>
+	</Row>
 )
 export default TimeRow
