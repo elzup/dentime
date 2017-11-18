8e642679e0ead100858ca19d41ecd1f565cc13db
elzup <guild0105@gmail.com>
2017-11-19 06:35:41 +0900

    Service worker

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 import './config/initialize'
+import registerServiceWorker from './config/registerServiceWorker'
 
 const root = document.getElementById('root')
 if (root !== null) {
 	ReactDOM.render(<App />, root)
+	registerServiceWorker()
 }
