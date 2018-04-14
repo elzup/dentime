d484b313f59331affe0fa0c262b0cc22f7ab8053
elzup <guild0105@gmail.com>
2018-04-14 15:42:18 +0900

    Update for offline data

--- src/api/index.js
+++ src/api/index.js
@@ -3,8 +3,7 @@
 import type { TimeResponse } from '../types'
 import axios from 'axios'
 
-const DATA_URL =
-	'https://raw.githubusercontent.com/elzup/db/master/tdu/time.json'
+const DATA_URL = '/time.json'
 
 export async function loadData(): Promise<TimeResponse> {
 	const res = await axios.get(DATA_URL)
