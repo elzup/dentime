883d0e57d6fd5bf2dc6aa1a0102e775e0f97e9c0
elzup <guild0105@gmail.com>
2019-10-07 18:09:47 +0900

    test: fix time

--- src/components/Board/index.test.js
+++ src/components/Board/index.test.js
@@ -3,6 +3,7 @@ import React from 'react'
 import Board from '.'
 import renderer from 'react-test-renderer'
 import periods from './testdata'
+import 'moment/locale/ja'
 
 it('renders correctly', () => {
 	const tree = renderer.create(<Board periods={periods} />).toJSON()
