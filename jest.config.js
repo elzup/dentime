040f48c1e14e5561c9ad270c29043751fe120ed2
elzup <guild0105@gmail.com>
2021-08-25 21:36:31 +0900

    update packages

--- jest.config.js
+++ jest.config.js
@@ -9,11 +9,11 @@ module.exports = {
 		'^.+\\.(ts|tsx)$': 'ts-jest',
 	},
 	testMatch: ['**/*.test.(ts|tsx)'],
-	setupFiles: ['./jest.setup.js'],
 	testPathIgnorePatterns: ['./.next/', './node_modules/'],
 	globals: {
 		'ts-jest': {
-			tsConfig: 'tsconfig.jest.json',
+			tsconfig: 'tsconfig.jest.json',
 		},
 	},
+	setupFilesAfterEnv: ['./jest.setup.js'],
 }
--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,5 @@
 import { configure } from 'enzyme'
 import ReactSixteenAdapter from 'enzyme-adapter-react-16'
+import '@babel/polyfill'
 
 configure({ adapter: new ReactSixteenAdapter() })
